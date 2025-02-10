39ee2697afc0c91b3644e118f20a6849c9e4fb01
M <114847810+riraum@users.noreply.github.com>
2025-02-10 20:42:38 +0700

    Add relative time method

--- main.go
+++ main.go
@@ -21,3 +21,18 @@ type Items struct {
 func main() {
 	fmt.Println("Hello hn-cli")
 }
+
+/*
+compare current time and posting time
+if difference of posting time and current time is <= 1h return 1h
+Figure out how to get 1hr, define it
+*/
+func (t Item) relativeTime() string {
+	// now := time.Now()
+	var returnValue string
+	h, _ := time.ParseDuration("1h")
+	if time.Duration(t.time.Hour()) < h {
+		returnValue = "<1h"
+	}
+	return returnValue
+}
