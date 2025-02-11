5652c81b636b291fabfaa73aaa2c96578ec24ff0
M <114847810+riraum@users.noreply.github.com>
2025-02-11 17:51:28 +0700

    WIP return time values

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ type Item struct {
 }
 
 type Items struct {
-	Item struct {
+	Item []struct {
 	}
 }
 
@@ -29,15 +29,37 @@ Figure out how to get 1hr, define it
 */
 func (t Item) relativeTime() string {
 	postTime := t.time
-	now := time.Now()
+	// now := time.Now()
 	var returnValue string
-	h, _ := time.ParseDuration("1h")
-	d, _ := time.ParseDuration("1d")
-	m, _ := time.ParseDuration("1m")
-	y, _ := time.ParseDuration("1y")
-	postAge := now.Sub(postTime)
-	// if now-t.time < h {
-	// 	returnValue = "<1h"
+	// h, _ := time.ParseDuration("1h")
+	// d, _ := time.ParseDuration("1d")
+	// m, _ := time.ParseDuration("1m")
+	// y, _ := time.ParseDuration("1y")
+	postAge := time.Since(postTime)
+	elapsedTime, _ := time.ParseDuration(postAge.String())
+	elapsedHours := elapsedTime.Hours()
+
+	// <  1hr
+	if elapsedHours < 1 {
+		return "<1h"
+	}
+	// number of hours, <24
+	if elapsedHours < 24 {
+		hrs, rest := "%f h", elapsedHours
+		combine := hrs + fmt.Sprint(rest)
+		return combine /* ... formatting with "Nh" ... */
+	}
+	// number of days, <30
+	if elapsedHours < 720 {
+		return /* ... formatting with "Nd" ... */
+	}
+	// number of months, <12
+	if elapsedHours < 8640 {
+		return /* ... formatting with "Nm" ... */
+	}
+	// number of years
+	// if elapsedHours > 8640 {
+	// 	return /* ... formatting with "Ny" ... */
 	// }
-	return returnValue
+	return /* ... formatting with "Ny" ... */
 }
--- main_test.go
+++ main_test.go
@@ -1,23 +1,24 @@
 package main
 
-import (
-	"testing"
-)
+// func TestRelativeTime(t *testing.T) {
+// 	tests := []struct {
+// 		item Item
+// 		want string
+// 	}{
+// 		{
+// 			item: Item{
+// 				title:  "Random title",
+// 				author: "Mr Crabs",
+// 				score:  1337,
+// 				time:   2009-11-10 23:00:00 +0000 UTC m=+0.000000001",
+// 				URL:    "example.com/404",
+// 			},
+// 			want: "1",
+// 		},
+// 	}
 
-func TestRelativeTime(t *testing.T) {
-	tests := []struct {
-		item Item
-		want string
-	}{
-		item: Item{
-			title: "Random title",
-			author: "Mr Crabs",
-			score: "1337",
-			time: "1136239445",
-			URL: "example.com/404",
-		},
-		want: "1",
-	},
+// 	for _, test := range tests {
 
+// 	}
 
-	}
+// }
