8f7bc09ef0b6bc2fae26f1bc16929aae46a96ffb
M <114847810+riraum@users.noreply.github.com>
2025-02-14 16:11:42 +0700

    WIP change test to hoursSincePosting logic

--- main_test.go
+++ main_test.go
@@ -2,53 +2,57 @@ package main
 
 import (
 	"testing"
-	"time"
 )
 
 func TestRelativeTime(t *testing.T) {
 	tests := []struct {
 		item Item
-		want time.Duration
+		want string
 	}{
 		{
 			item: Item{
 				// title:  "Random title",
 				// author: "Mr Crabs",
 				// score:  0,
-				absolutePostTime: time.Date(2024, 8, 13, 14, 30, 45, 100, time.UTC),
+				// absolutePostTime: time.Date(2024, 8, 13, 14, 30, 45, 100, time.UTC),
+				hoursSincePosting: 4433,
 				// URL:    "example.com/404",
 			},
-			// want: "6m",
+			want: "6m",
 		},
 		{
 			item: Item{
-				absolutePostTime: time.Date(2025, 1, 01, 14, 30, 45, 100, time.UTC),
+				// absolutePostTime: time.Date(2025, 1, 01, 14, 30, 45, 100, time.UTC),
+				hoursSincePosting: 1049,
 			},
-			// want: "1m",
+			want: "1m",
 		},
 		{
 			item: Item{
-				absolutePostTime: time.Date(2025, 2, 01, 14, 30, 45, 100, time.UTC),
+				// absolutePostTime: time.Date(2025, 2, 01, 14, 30, 45, 100, time.UTC),
+				hoursSincePosting: 305,
 			},
-			// want: "12d",
+			want: "12d",
 		},
 		{
 			item: Item{
-				absolutePostTime: time.Date(2024, 2, 01, 14, 30, 45, 100, time.UTC),
+				// absolutePostTime: time.Date(2024, 2, 01, 14, 30, 45, 100, time.UTC),
+				hoursSincePosting: 9089,
 			},
-			// want: "1y",
+			want: "1y",
 		},
 	}
 
 	for _, test := range tests {
-		now := time.Now()
-		diff := now.Sub(test.item.absolutePostTime)
-		test.want = diff
+		// now := time.Now()
+		// diff := now.Sub(test.item.absolutePostTime)
+		// test.want = diff
 
-		test.item.addHoursSincePosting()
+		// test.item.addHoursSincePosting()
 		// absoluteTime := Item{absolutePostTime: test.item.absolutePostTime}
-		got := test.item.hoursSincePosting
-
+		// got := test.item.hoursSincePosting
+		// x := test.item
+		got := test.item.relativeTime()
 		// absoluteTime := Item{absolutePostTime: test.item.absolutePostTime}
 		// got := absolutePostTime.relativeTime()
 
