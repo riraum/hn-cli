85ab6163a34a81e3ac74eb93fc6ea2559f579595
M <114847810+riraum@users.noreply.github.com>
2025-02-14 23:11:30 +0700

    Remove test code

--- main_test.go
+++ main_test.go
@@ -14,8 +14,6 @@ func TestRelativeTime(t *testing.T) {
 				// title:  "Random title",
 				// author: "Mr Crabs",
 				// score:  0,
-				// absolutePostTime: time.Date(2024, 8, 13, 14, 30, 45, 100, time.UTC),
-				// hoursSincePosting: "4435h35m7.2785059s",
 				timeSincePosting: 15966000000000000,
 				// URL:    "example.com/404",
 			},
@@ -23,24 +21,18 @@ func TestRelativeTime(t *testing.T) {
 		},
 		{
 			item: Item{
-				// absolutePostTime: time.Date(2025, 1, 01, 14, 30, 45, 100, time.UTC),
-				// hoursSincePosting: 1051h35m7.2785799s,
 				timeSincePosting: 3783600000000000,
 			},
 			want: "1m",
 		},
 		{
 			item: Item{
-				// absolutePostTime: time.Date(2025, 2, 01, 14, 30, 45, 100, time.UTC),
-				// hoursSincePosting: 307h35m7.2785829s,
 				timeSincePosting: 1105200000000000,
 			},
 			want: "13d",
 		},
 		{
 			item: Item{
-				// absolutePostTime: time.Date(2024, 2, 01, 14, 30, 45, 100, time.UTC),
-				// hoursSincePosting: 9091h35m7.2786449s,
 				timeSincePosting: 32727599999999996,
 			},
 			want: "1y",
@@ -49,8 +41,6 @@ func TestRelativeTime(t *testing.T) {
 
 	for _, test := range tests {
 		got := test.item.relativeTime()
-		// debug
-		// fmt.Println(test.item.timeSincePosting)
 		if got != test.want {
 			t.Errorf("Got: %v, want: %v", got, test.want)
 		}
