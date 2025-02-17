2234b504270a3f12bb2c8264ed739cbc056780d2
M <114847810+riraum@users.noreply.github.com>
2025-02-17 18:54:00 +0700

    Unmarshal data

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ type Item struct {
 	Title  string `json:"title"`
 	Author string `json:"by"`
 	Score  int    `json:"score"`
+	Id     int    `json:"id"`
 	// time   time.Time
 	// absolutePostTime  time.Time
 	timeSincePosting time.Duration
@@ -26,7 +27,7 @@ type Item struct {
 type Items []Item
 
 type HNItem struct {
-	id          json.Number
+	id          int
 	by          string
 	descendants json.Number
 	kids        []json.Number
@@ -40,68 +41,85 @@ type HNItems []HNItem
 
 var HItem Item
 var HItems Items
-var mf []interface{}
-
-var HNPost HNItem
-var HNFrontpage HNItems
-
-var f interface{}
-var fs []interface{}
-
-// var dat []interface{}
-
-// var test Item
 
 func main() {
 	fmt.Println("Hello hn-cli")
 
-	// item1 := Items{}
-	testJSON := Item{"Alice in Wonderland", "Bob", 666, 15966000000000000, "example.com/404"}
+	// testJSON := Item{"Alice in Wonderland", "Bob", 666, 1337, 15966000000000000, "example.com/404"}
 
-	var storeData Item
-	marshallData, mErr := json.Marshal(testJSON)
-	if mErr != nil {
-		fmt.Println(mErr)
-	}
-	fmt.Println(string(marshallData))
+	// testPost, testErr := json.Marshal(Item{
+	// 	// "by" : "dhouston",
+	// 	// "descendants" : 71,
+	// 	id: 8863,
+	// 	// "score" : 111,
+	// 	// "time" : 1175714200,
+	// 	// "title" : "My YC app: Dropbox - Throw away your USB drive",
+	// 	// "type" : "story",
+	// 	// "url" : "http://www.getdropbox.com/u/2/screencast.html"
+	// })
 
-	unmarschallErr := json.Unmarshal(marshallData, &storeData)
-	if unmarschallErr != nil {
-		fmt.Println(unmarschallErr)
-	}
-	// marshallData == []byte(`{"Title":"Alice in Wonderland","Author":"Bob","TimeSincePosting":15966000000000000}`)
-	fmt.Println(storeData)
-
-	// var unmarshallData Item
-	// unmarshallErr := json.Unmarshal(marshallData, &unmarshallData)
-	// if unmarshallErr != nil {
-	// 	fmt.Println(unmarshallErr)
+	// if testErr != nil {
+	// 	panic(testErr)
 	// }
-	// fmt.Println(unmarshallData)
-	// debug
-	// fmt.Println(test)
-	// testBytes := []byte(test)
-	// fmt.Println(string(testBytes))
 
-	// merr := json.Unmarshal(testBytes, &item1)
-	// if merr != nil {
-	// 	fmt.Println(merr)
+	// fmt.Println(string(testPost))
+
+	// // uErr := json.Unmarshal(testPost, &HItem)
+	// // if uErr != nil {
+	// // 	panic(uErr)
+	// // }
+	// // fmt.Println(HItem)
+
+	// testPosts, testsErr := json.Marshal(Items{Item{
+	// 	// "by" : "dhouston",
+	// 	// "descendants" : 71,
+	// 	id: 8863,
+	// 	// "score" : 111,
+	// 	// "time" : 1175714200,
+	// 	// "title" : "My YC app: Dropbox - Throw away your USB drive",
+	// 	// "type" : "story",
+	// 	// "url" : "http://www.getdropbox.com/u/2/screencast.html"
+	// },
+	// 	Item{
+	// 		// "by" : "dhouston",
+	// 		// "descendants" : 71,
+	// 		id: 8865,
+	// 		// "score" : 111,
+	// 		// "time" : 1175714200,
+	// 		// "title" : "My YC app: Dropbox - Throw away your USB drive",
+	// 		// "type" : "story",
+	// 		// "url" : "http://www.getdropbox.com/u/2/screencast.html"
+	// 	}})
+	// if testsErr != nil {
+	// 	panic(testsErr)
 	// }
-	// fmt.Println(item1)
+	// fmt.Println(string(testPosts))
 
-	// marshallTest, mtErr := json.Marshal(Item{timeSincePosting: 15966000000000000})
-	// if mtErr != nil {
-	// 	panic(mtErr)
-	// }
-	// fmt.Println(string(marshallTest))
-
-	errj := json.Unmarshal(body, &HItems)
-	if errj != nil {
-		panic(errj)
+	testJSONPost := []byte(`{"id":8863}`)
+	err := json.Unmarshal(testJSONPost, &HItem)
+	if err != nil {
+		panic(err)
 	}
-	// debug
-	fmt.Println(HItems)
-	// fmt.Println(item1.)
+	fmt.Println(HItem)
+
+	// testJSONPosts := []byte(`{
+	// 	"id": 8863},
+	// 		"id": 8865,
+	// 	}}`)
+
+	// var storeData Item
+	// marshallData, mErr := json.Marshal(testJSON)
+	// if mErr != nil {
+	// 	fmt.Println(mErr)
+	// }
+	// fmt.Println(string(marshallData))
+
+	// unmarschallErr := json.Unmarshal(marshallData, &storeData)
+	// if unmarschallErr != nil {
+	// 	fmt.Println(unmarschallErr)
+	// }
+	// fmt.Println(storeData)
+
 }
 
 //	func (t Item) addHoursSincePosting() time.Duration {
