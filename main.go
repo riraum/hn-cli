750715976d66b902ced81bdd3105d054baf23fc5
M <114847810+riraum@users.noreply.github.com>
2025-02-17 13:24:34 +0700

    WIP playing with un and marshall

--- main.go
+++ main.go
@@ -3,9 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io"
 	"math"
-	"net/http"
 	"time"
 )
 
@@ -16,8 +14,8 @@ const (
 )
 
 type Item struct {
-	// title  string
-	// author string
+	Title  string
+	Author string
 	// score  int
 	// absolutePostTime  time.Time
 	timeSincePosting time.Duration
@@ -28,33 +26,70 @@ type Items []Item
 
 var m Item
 var ms Items
-var mf interface{}
+var mf map[string]interface{}
+
+// var test Item
 
 func main() {
 	fmt.Println("Hello hn-cli")
 
-	marshallTest, mtErr := json.Marshal(Item{timeSincePosting: 15966000000000000})
-	if mtErr != nil {
-		panic(mtErr)
-	}
-	fmt.Println(marshallTest)
+	// item1 := Items{}
+	testJSON := Item{"Alice in Wonderland", "Bob", 15966000000000000}
 
-	resp, err := http.Get("https://hacker-news.firebaseio.com/v0/topstories.json")
-	if err != nil {
-		panic(err)
+	var storeData Item
+	marshallData, mErr := json.Marshal(testJSON)
+	if mErr != nil {
+		fmt.Println(mErr)
 	}
+	fmt.Println(marshallData)
 
-	defer resp.Body.Close()
-	body, _ := io.ReadAll(resp.Body)
+	unmarschallErr := json.Unmarshal(marshallData, &storeData)
+	if unmarschallErr != nil {
+		fmt.Println(unmarschallErr)
+	}
+	// marshallData == []byte(`{"Title":"Alice in Wonderland","Author":"Bob","TimeSincePosting":15966000000000000}`)
+	fmt.Println(storeData)
+
+	// var unmarshallData Item
+	// unmarshallErr := json.Unmarshal(marshallData, &unmarshallData)
+	// if unmarshallErr != nil {
+	// 	fmt.Println(unmarshallErr)
+	// }
+	// fmt.Println(unmarshallData)
 	// debug
-	fmt.Println(body)
+	// fmt.Println(test)
+	// testBytes := []byte(test)
+	// fmt.Println(string(testBytes))
 
-	errj := json.Unmarshal(body, ms)
-	if errj != nil {
-		panic(errj)
-	}
+	// merr := json.Unmarshal(testBytes, &item1)
+	// if merr != nil {
+	// 	fmt.Println(merr)
+	// }
+	// fmt.Println(item1)
+
+	// marshallTest, mtErr := json.Marshal(Item{timeSincePosting: 15966000000000000})
+	// if mtErr != nil {
+	// 	panic(mtErr)
+	// }
+	// fmt.Println(string(marshallTest))
+
+	// resp, err := http.Get("https://hacker-news.firebaseio.com/v0/topstories.json")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer resp.Body.Close()
+
+	// body, _ := io.ReadAll(resp.Body)
+	// // debug
+	// fmt.Println(body)
+
+	// errj := json.Unmarshal(body, &item1)
+	// if errj != nil {
+	// 	panic(errj)
+	// }
 	// debug
-	fmt.Println(mf)
+	// fmt.Println(mf)
+	// fmt.Println(item1.)
 }
 
 //	func (t Item) addHoursSincePosting() time.Duration {
