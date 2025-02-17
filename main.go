c1d478c4693ade7c3dddd9d06934718534977c47
M <114847810+riraum@users.noreply.github.com>
2025-02-17 17:08:15 +0700

    Remove marshall code

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"io"
 	"math"
@@ -27,75 +26,8 @@ type Item struct {
 
 type Items []Item
 
-type HNItem struct {
-	id          json.Number
-	by          string
-	descendants json.Number
-	kids        []json.Number
-	score       json.Number
-	time        time.Time
-	url         string
-	title       string
-}
-
-type HNItems []HNItem
-
-var HItem Item
-var HItems Items
-var mf []interface{}
-
-var HNPost HNItem
-var HNFrontpage HNItems
-
-var f interface{}
-var fs []interface{}
-
-// var dat []interface{}
-
-// var test Item
-
 func main() {
 	fmt.Println("Hello hn-cli")
-
-	// item1 := Items{}
-	testJSON := Item{"Alice in Wonderland", "Bob", 666, 15966000000000000, "example.com/404"}
-
-	var storeData Item
-	marshallData, mErr := json.Marshal(testJSON)
-	if mErr != nil {
-		fmt.Println(mErr)
-	}
-	fmt.Println(string(marshallData))
-
-	unmarschallErr := json.Unmarshal(marshallData, &storeData)
-	if unmarschallErr != nil {
-		fmt.Println(unmarschallErr)
-	}
-	// marshallData == []byte(`{"Title":"Alice in Wonderland","Author":"Bob","TimeSincePosting":15966000000000000}`)
-	fmt.Println(storeData)
-
-	// var unmarshallData Item
-	// unmarshallErr := json.Unmarshal(marshallData, &unmarshallData)
-	// if unmarshallErr != nil {
-	// 	fmt.Println(unmarshallErr)
-	// }
-	// fmt.Println(unmarshallData)
-	// debug
-	// fmt.Println(test)
-	// testBytes := []byte(test)
-	// fmt.Println(string(testBytes))
-
-	// merr := json.Unmarshal(testBytes, &item1)
-	// if merr != nil {
-	// 	fmt.Println(merr)
-	// }
-	// fmt.Println(item1)
-
-	// marshallTest, mtErr := json.Marshal(Item{timeSincePosting: 15966000000000000})
-	// if mtErr != nil {
-	// 	panic(mtErr)
-	// }
-	// fmt.Println(string(marshallTest))
 
 	resp, err := http.Get("https://hacker-news.firebaseio.com/v0/topstories.json")
 	if err != nil {
@@ -106,14 +38,6 @@ func main() {
 	body, _ := io.ReadAll(resp.Body)
 	// debug
 	fmt.Println(string(body))
-
-	errj := json.Unmarshal(body, &HItems)
-	if errj != nil {
-		panic(errj)
-	}
-	// debug
-	fmt.Println(HItems)
-	// fmt.Println(item1.)
 }
 
 //	func (t Item) addHoursSincePosting() time.Duration {
