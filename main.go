6c70578d87bb7653c49c8e8ad47ba67a97198e85
M <114847810+riraum@users.noreply.github.com>
2025-02-17 20:23:14 +0700

    Revert "Satisfy revive, struct key rename"
    This reverts commit 2801d4c0368301459c31a0dff376f106387a0cc4.

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ type Item struct {
 	Title  string `json:"title"`
 	Author string `json:"by"`
 	Score  int    `json:"score"`
-	ID     int    `json:"id"`
+	Id     int    `json:"id"`
 	// time   time.Time
 	// absolutePostTime  time.Time
 	timeSincePosting time.Duration
@@ -60,16 +60,13 @@ func main() {
 
 func Unmarshal(input string) Item {
 	stringBytes := []byte(input)
-
 	var output Item
-
 	unmarshallErr := json.Unmarshal(stringBytes, &output)
 	if unmarshallErr != nil {
 		panic(unmarshallErr)
 	}
 	// debug
 	fmt.Println(output)
-
 	return output
 }
 
