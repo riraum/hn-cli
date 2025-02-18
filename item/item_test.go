3d2d88a5b43cd93d02ec553c969fd8e03561f9f2
M <114847810+riraum@users.noreply.github.com>
2025-02-18 20:26:59 +0700

    Move item code to package

---
+++ item/item_test.go
@@ -0,0 +1,171 @@
+package item
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestRelativeTime(t *testing.T) {
+	tests := []struct {
+		item Item
+		want string
+	}{
+		{
+			item: Item{
+				timeSincePosting: 15966000000000000,
+			},
+			want: "6m",
+		},
+		{
+			item: Item{
+				timeSincePosting: 3783600000000000,
+			},
+			want: "1m",
+		},
+		{
+			item: Item{
+				timeSincePosting: 1105200000000000,
+			},
+			want: "13d",
+		},
+		{
+			item: Item{
+				timeSincePosting: 32727599999999996,
+			},
+			want: "1y",
+		},
+	}
+
+	for _, test := range tests {
+		got := test.item.relativeTime()
+		if got != test.want {
+			t.Errorf("Got: %v, want: %v", got, test.want)
+		}
+	}
+}
+
+func TestMarshall(t *testing.T) {
+	tests := []struct {
+		dataToMarshall Item
+		want           []byte
+	}{
+		{
+			dataToMarshall: Item{
+				Title:  "Alice in Wonderland",
+				Author: "Lewis Carroll",
+			},
+			want: []byte(`{"title":"Alice in Wonderland","by":"Lewis Carroll"}`),
+		},
+		{
+			dataToMarshall: Item{
+				Title:  "",
+				Author: "Marcus Aurelius",
+			},
+			want: []byte(`{"title":"","by":"Marcus Aurelius"}`),
+		},
+		{
+			dataToMarshall: Item{
+				Title:  "Meditations",
+				Author: "",
+			},
+			want: []byte(`{"title":"Meditations","by":""}`),
+		},
+		{
+			dataToMarshall: Item{
+				Title:  "",
+				Author: "",
+			},
+			want: []byte(`{"title":"","by":""}`),
+		},
+		{
+			dataToMarshall: Item{
+				Author: "Marcus Aurelius",
+			},
+			want: []byte(`{"title":"","by":"Marcus Aurelius"}`),
+		},
+		{
+			dataToMarshall: Item{
+				Title: "Meditations",
+			},
+			want: []byte(`{"title":"Meditations","by":""}`),
+		},
+		{
+			dataToMarshall: Item{},
+			want:           []byte(`{"title":"","by":""}`),
+		},
+	}
+
+	for _, test := range tests {
+		got, err := Marshall(test.dataToMarshall)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if !bytes.Equal(got, test.want) {
+			t.Errorf("Got: %v, want: %v", string(got), string(test.want))
+		}
+	}
+}
+
+func TestUnmarshall(t *testing.T) {
+	tests := []struct {
+		dataToUnmarshall []byte
+		want             Item
+	}{
+		{
+			dataToUnmarshall: []byte(`{"title":"Alice in Wonderland","by":"Lewis Carroll"}`),
+			want: Item{
+				Title:  "Alice in Wonderland",
+				Author: "Lewis Carroll",
+			},
+		},
+		{
+			dataToUnmarshall: []byte(`{"title":"Meditations"}`),
+			want: Item{
+				Title: "Meditations",
+			},
+		},
+		{
+			dataToUnmarshall: []byte(`{"by":"Marcus Aurelius"}`),
+			want: Item{
+				Author: "Marcus Aurelius",
+			},
+		},
+		{
+			dataToUnmarshall: []byte(`{"title":"Meditations","by":""}`),
+			want: Item{
+				Author: "",
+				Title:  "Meditations",
+			},
+		},
+		{
+			dataToUnmarshall: []byte(`{"title":"","by":"Marcus Aurelius"}`),
+			want: Item{
+				Author: "Marcus Aurelius",
+				Title:  "",
+			},
+		},
+		{
+			dataToUnmarshall: []byte(`{"title":"","by":""}`),
+			want: Item{
+				Author: "",
+				Title:  "",
+			},
+		},
+		{
+			dataToUnmarshall: []byte(`{}`),
+			want:             Item{},
+		},
+	}
+
+	for _, test := range tests {
+		got, err := Unmarshal(test.dataToUnmarshall)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if got != test.want {
+			t.Errorf("Got: %v, want: %v", got, test.want)
+		}
+	}
+}
--- main.go
+++ main.go
@@ -1,98 +1,27 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"math"
-	"time"
+
+	"github.com/riraum/hn/cli/item"
 )
-
-const (
-	hoursInADay   = 24
-	hoursInAMonth = 730
-	hoursInAYear  = 8760
-)
-
-//nolint:all
-type Item struct {
-	Title  string `json:"title"`
-	Author string `json:"by"`
-	// Score  int    `json:"score"`
-	// ID     int    `json:"id"`
-	absolutePostTime  time.Time
-	timeSincePosting  time.Duration
-	hoursSincePosting time.Duration
-	// URL string `json:"url"`
-}
-
-// ...
-type Items []Item
 
 func main() {
 	fmt.Println("Hello hn-cli user")
 
-	dataToMarshall := Item{Title: "Alice in Wonderland", Author: "Lewis Carroll"}
+	dataToMarshall := item.Item{Title: "Alice in Wonderland", Author: "Lewis Carroll"}
 
-	dataMarshalled, mErr := Marshall(dataToMarshall)
+	dataMarshalled, mErr := item.Marshall(dataToMarshall)
 	if mErr != nil {
 		panic(mErr)
 	}
 	// debug
 	fmt.Println(dataMarshalled)
 
-	dataUnmarshalled, uErr := Unmarshal(dataMarshalled)
+	dataUnmarshalled, uErr := item.Unmarshal(dataMarshalled)
 	if uErr != nil {
 		panic(uErr)
 	}
 	// debug
 	fmt.Println(dataUnmarshalled)
-}
-
-func Marshall(input Item) ([]byte, error) {
-	dataMarshalled, err := json.Marshal(input)
-	if err != nil {
-		return dataMarshalled, err
-	}
-
-	return dataMarshalled, nil
-}
-
-func Unmarshal(input []byte) (Item, error) {
-	var dataUnmarshalled Item
-
-	err := json.Unmarshal(input, &dataUnmarshalled)
-	if err != nil {
-		return dataUnmarshalled, err
-	}
-
-	return dataUnmarshalled, nil
-}
-
-//nolint:all
-func (t Item) addHoursSincePosting() time.Duration {
-	t.hoursSincePosting = time.Since(t.absolutePostTime)
-	return t.hoursSincePosting
-}
-
-// ...
-func (t Item) relativeTime() string {
-	elapsedHours := t.timeSincePosting.Hours()
-
-	if elapsedHours < 1 {
-		return "<1h"
-	}
-
-	if elapsedHours < hoursInADay {
-		return fmt.Sprint(math.Round(elapsedHours), "h")
-	}
-
-	if elapsedHours < hoursInAMonth {
-		return fmt.Sprint(math.Round(elapsedHours/hoursInADay), "d")
-	}
-
-	if elapsedHours < hoursInAYear {
-		return fmt.Sprint(math.Round(elapsedHours/hoursInAMonth), "m")
-	}
-
-	return fmt.Sprint(math.Round(elapsedHours/hoursInAYear), "y")
 }
