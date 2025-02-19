cce918ac1e56e1b2a6d848d9e3b5155dfbd7d162
M <114847810+riraum@users.noreply.github.com>
2025-02-19 21:09:42 +0700

    ADjust comment

--- main.go
+++ main.go
@@ -54,25 +54,26 @@ func main() {
 	}
 	// debug
 	fmt.Println(postUnmarshalled)
-	//	for _, value := range frontpageJSON {
-	//		post, uFErr := item.Unmarshal(frontpageJSON)
-	//		if uFErr != nil {
-	//			panic(uFErr)
-	//		}
-	//		frontpage = post.Title
-	//	}
-	//
-	// fmt.Println(post)
-	//
-	//	for _, value := range frontpageJSON {
-	//		post, uFErr := item.Unmarshal(frontpageJSON)
-	//		if uFErr != nil {
-	//			panic(uFErr)
-	//		}
-	//		fmt.Println(post)
-	//		frontpage = post
-	//	}
-	//
-	// fmt.Println(frontpage)
-	// fmt.Println((unmarshallFrontpage))
 }
+
+//	for _, value := range frontpageJSON {
+//		post, uFErr := item.Unmarshal(frontpageJSON)
+//		if uFErr != nil {
+//			panic(uFErr)
+//		}
+//		frontpage = post.Title
+//	}
+//
+// fmt.Println(post)
+//
+//	for _, value := range frontpageJSON {
+//		post, uFErr := item.Unmarshal(frontpageJSON)
+//		if uFErr != nil {
+//			panic(uFErr)
+//		}
+//		fmt.Println(post)
+//		frontpage = post
+//	}
+//
+// fmt.Println(frontpage)
+// fmt.Println((unmarshallFrontpage))
