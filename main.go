d73ca1881329b45254525f6882f00ea9c9806e52
M <114847810+riraum@users.noreply.github.com>
2025-02-20 18:24:38 +0700

    Comment out sample code

--- main.go
+++ main.go
@@ -10,22 +10,21 @@ import (
 
 func main() {
 	fmt.Println("Hello hn-cli user")
+	// dataToMarshall := item.Item{Title: "Alice in Wonderland", Author: "Lewis Carroll"}
 
-	dataToMarshall := item.Item{Title: "Alice in Wonderland", Author: "Lewis Carroll"}
+	// dataMarshalled, mErr := item.Marshall(dataToMarshall)
+	// if mErr != nil {
+	// 	panic(mErr)
+	// }
+	// // debug
+	// fmt.Println(string(dataMarshalled))
 
-	dataMarshalled, mErr := item.Marshall(dataToMarshall)
-	if mErr != nil {
-		panic(mErr)
-	}
-	// debug
-	fmt.Println(string(dataMarshalled))
-
-	dataUnmarshalled, uErr := item.Unmarshal(dataMarshalled)
-	if uErr != nil {
-		panic(uErr)
-	}
-	// debug
-	fmt.Println(dataUnmarshalled)
+	// dataUnmarshalled, uErr := item.Unmarshal(dataMarshalled)
+	// if uErr != nil {
+	// 	panic(uErr)
+	// }
+	// // debug
+	// fmt.Println(dataUnmarshalled)
 	// API code below
 	frontpageJSON := http.GetJSON("https://hacker-news.firebaseio.com/v0/topstories.json")
 	// debug
