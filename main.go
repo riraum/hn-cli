c274fcad763afed8519da6a7404b69e4b2e2d2fc
M <114847810+riraum@users.noreply.github.com>
2025-02-20 18:03:28 +0700

    Unmarshall into slice of ints

--- main.go
+++ main.go
@@ -11,27 +11,27 @@ import (
 func main() {
 	fmt.Println("Hello hn-cli user")
 
-	// dataToMarshall := item.Item{Title: "Alice in Wonderland", Author: "Lewis Carroll"}
+	dataToMarshall := item.Item{Title: "Alice in Wonderland", Author: "Lewis Carroll"}
 
-	// dataMarshalled, mErr := item.Marshall(dataToMarshall)
-	// if mErr != nil {
-	// 	panic(mErr)
-	// }
-	// // debug
-	// fmt.Println(string(dataMarshalled))
-
-	// dataUnmarshalled, uErr := item.Unmarshal(dataMarshalled)
-	// if uErr != nil {
-	// 	panic(uErr)
-	// }
+	dataMarshalled, mErr := item.Marshall(dataToMarshall)
+	if mErr != nil {
+		panic(mErr)
+	}
 	// debug
-	// fmt.Println(dataUnmarshalled)
+	fmt.Println(string(dataMarshalled))
+
+	dataUnmarshalled, uErr := item.Unmarshal(dataMarshalled)
+	if uErr != nil {
+		panic(uErr)
+	}
+	// debug
+	fmt.Println(dataUnmarshalled)
 	// API code below
 	frontpageJSON := http.GetJSON("https://hacker-news.firebaseio.com/v0/topstories.json")
 	// debug
 	fmt.Println(string(frontpageJSON))
 
-	var frontpage item.Items
+	var frontpageIDs []int
 
 	// for _, value := range frontpageJSON {
 	// 	err := json.Unmarshal(value, &frontpage)
@@ -40,11 +40,11 @@ func main() {
 	// 	}
 	// }
 
-	err := json.Unmarshal(frontpageJSON, &frontpage)
+	err := json.Unmarshal(frontpageJSON, &frontpageIDs)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(frontpage)
+	fmt.Println(frontpageIDs)
 
 	// frontpageJSONList, fpErr := item.Unmarshal(frontpageJSON)
 	// if fpErr != nil {
