8abd378e52472911a2a63b6b8f8a24195c5b204d
M <114847810+riraum@users.noreply.github.com>
2025-02-22 23:57:33 +0700

    Simplify

--- item/item.go
+++ item/item.go
@@ -49,9 +49,9 @@ func Unmarshal(input []byte) (Item, error) {
 
 // ...
 func (t Item) RelativeTime() string {
-	absolutePostTime := time.Unix(t.UnixPostTime, 0)
-	timesincePosting := time.Since(absolutePostTime)
-	elapsedHours := timesincePosting.Hours()
+	// absolutePostTime := time.Unix(t.UnixPostTime, 0)
+	// timesincePosting := time.Since(absolutePostTime)
+	elapsedHours := time.Since(time.Unix(t.UnixPostTime, 0)).Hours()
 
 	if elapsedHours < 1 {
 		return "<1h"
