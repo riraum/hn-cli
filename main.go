9576afbed6c0517a9fee0d4a4cde4e04a31cad82
M <114847810+riraum@users.noreply.github.com>
2025-02-26 00:14:14 +0700

    Add back hours

--- main.go
+++ main.go
@@ -34,6 +34,7 @@ func main() {
 	var timeConvert item.Item
 	// set initial time as int64
 	timeConvert.UnixPostTime = 1494505756
+	timeConvert.HoursSincePosting = timeConvert.AddHoursSincePosting()
 	timeConvert.FormattedTime = timeConvert.RelativeTime()
 	fmt.Println(timeConvert)
 	// Get terminal size test code
@@ -70,6 +71,8 @@ func main() {
 		if pErr != nil {
 			panic(pErr)
 		}
+
+		postUnmarshalled.HoursSincePosting = postUnmarshalled.AddHoursSincePosting()
 
 		postUnmarshalled.FormattedTime = postUnmarshalled.RelativeTime()
 
