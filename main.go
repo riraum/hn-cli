9aaa05e07200d4652307c400cde3caaf9e67dafc
M <114847810+riraum@users.noreply.github.com>
2025-03-03 23:12:03 +0700

    Revert "Fix tests"
    This reverts commit dd2c89cce81268a46d8cb29d186ffa8ebc9614f3.

--- main.go
+++ main.go
@@ -46,7 +46,7 @@ func main() {
 
 	var postUnmarsh item.Item
 
-	for i := 0; i <= 10; i++ {
+	for i := 0; i <= 30; i++ {
 		postID := frontpageIDs[i]
 
 		postURL := fmt.Sprintf("https://hacker-news.firebaseio.com/v0/item/%v.json", postID)
@@ -63,10 +63,13 @@ func main() {
 
 		// Check for Ask/Show HN posts, without external URL
 		if postUnmarsh.ArticleURL == "" {
+			// frontpageID := frontpageIDs[i]
 			postUnmarsh.ArticleURL = fmt.Sprintf("https://news.ycombinator.com/item?id=%v", postID)
 		}
 
 		// Get CommentURL
+		// frontpageID := frontpageIDs[i]
+		// postUnmarsh.CommentURL = fmt.Sprintf("https://news.ycombinator.com/item?id=%v", frontpageID)
 		postUnmarsh.CommentURL = fmt.Sprintf("https://news.ycombinator.com/item?id=%v", postID)
 
 		// Get ArticleURL
@@ -75,7 +78,7 @@ func main() {
 		// postData := http.GetJSON(postURL)
 		// articleURL := fmt.Sprintf("https://hacker-news.firebaseio.com/v0/item/%v.json", postID)
 		// postData, err = http.GetJSON(articleURL)
-		// postUnmarsh.ArticleURL = articleURL
+		postUnmarsh.ArticleURL = postURL
 
 		postUnmarsh.Title = fmt.Sprintf("%.25s...", postUnmarsh.Title)
 		postUnmarsh.HoursSinceP = postUnmarsh.AddHoursSinceP()
@@ -107,18 +110,14 @@ func main() {
 	}
 
 	cmd := input[0]
-	// index := input[1]
 
-	var inputInt int
+	// var inputInt int
 
 	if len(input) >= hasIndex {
-		inputInt, err = strconv.Atoi(input[1])
-		if err != nil {
+		if _, err = strconv.Atoi(input[1]); err != nil {
 			panic(err)
 		}
 	}
-
-	fmt.Print(inputInt)
 
 	// To use once post print code is in function
 	if cmd == "start" {
@@ -136,6 +135,8 @@ func main() {
 	}
 	// Open comments cmd
 	if cmd == "comments" {
+		// frontpageID := frontpageIDs[inputInt]
+		// commentURL := fmt.Sprintf("https://news.ycombinator.com/item?id=%v", frontpageID)
 		if err := openLink(postUnmarsh.CommentURL); err != nil {
 			panic(err)
 		}
