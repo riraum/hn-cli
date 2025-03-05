3b1b4f14bd5d5d28da8ffd7a17b33e0699c88168
M <114847810+riraum@users.noreply.github.com>
2025-03-05 19:33:20 +0700

    Remove comments

--- format/format.go
+++ format/format.go
@@ -63,7 +63,5 @@ func Format(frontpageIDs []int, tWidth int) (item.Items, error) {
 		fmt.Println(index, postUnmarsh.Score, postUnmarsh.Author, postUnmarsh.Title, postUnmarsh.FormattedTime, "ago")
 	}
 
-	// fmt.Println("postUnmarshSlice", postUnmarshSlice)
-
 	return postUnmarshSlice, nil
 }
