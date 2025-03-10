239e7ed511665600b92716c7947b49d4d0632f64
M <114847810+riraum@users.noreply.github.com>
2025-03-10 14:04:48 +0700

    Add item struct test

--- http/http_test.go
+++ http/http_test.go
@@ -4,8 +4,11 @@ import (
 	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"reflect"
 	"slices"
 	"testing"
+
+	"github.com/riraum/hn-cli/item"
 )
 
 func TestGetJSON(t *testing.T) {
@@ -46,4 +49,37 @@ func TestGetJSON(t *testing.T) {
 		}
 	},
 	)
+	t.Run("Valid Item", func(t *testing.T) {
+		t.Parallel()
+
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+			w.WriteHeader(http.StatusOK)
+			fmt.Fprintln(w, `{
+  "by" : "dhouston",
+  "id" : 8863,
+  "score" : 111,
+  "time" : 1175714200,
+  "title" : "My YC app: Dropbox - Throw away your USB drive",
+  "url" : "http://www.getdropbox.com/u/2/screencast.html"
+}`)
+		}))
+		defer ts.Close()
+
+		var got item.Item
+
+		want := item.Item{Author: "dhouston",
+			Score:        111,
+			UnixPostTime: 1175714200,
+			Title:        "My YC app: Dropbox - Throw away your USB drive",
+			ArticleURL:   "http://www.getdropbox.com/u/2/screencast.html"}
+
+		err := GetJSON(ts.URL, &got)
+		if err != nil {
+			t.Fatalf("Expected no error, got %v", err)
+		}
+
+		if !reflect.DeepEqual(want, got) {
+			t.Fatalf("Expected %v, got %v", want, got)
+		}
+	})
 }
